73dab4c3ad9c40627cb5da31eb9165f221d51adc
Ezekiel Dominick Matias <ezekield236@gmail.com>
2023-07-05 21:08:43 -0500

    Finished JS file

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,51 @@
 // Assignment code here
+// Generate the password based on selected criteria
+function generatePassword() {
+  // Prompt the user for password criteria and validate the input
+  var length = prompt("Enter the length of the password (between 8 and 128 characters):");
+  length = parseInt(length); // Convert the input to a number
 
+  // Validate the password length
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Invalid password length. Please enter a number between 8 and 128.");
+    return ""; // Return an empty string to indicate an error
+  }
+
+  // Prompt for character types
+  var includeLowercase = confirm("Include lowercase letters?");
+  var includeUppercase = confirm("Include uppercase letters?");
+  var includeNumeric = confirm("Include numbers?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validate that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("You must include at least one character type.");
+    return ""; // Return an empty string to indicate an error
+  }
+
+  // Define the character pools based on the selected criteria
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+
+  // Create a pool of characters based on the selected criteria
+  var characterPool = "";
+  if (includeLowercase) characterPool += lowercaseChars;
+  if (includeUppercase) characterPool += uppercaseChars;
+  if (includeNumeric) characterPool += numericChars;
+  if (includeSpecial) characterPool += specialChars;
+
+  // Generate the password by randomly selecting characters from the pool
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * characterPool.length);
+    password += characterPool.charAt(randomIndex);
+  }
+
+  // Return the generated password
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
